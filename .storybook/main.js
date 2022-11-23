24a09a259a61320bdfbfea187603f88a993f100a
hhimanshu <dev.atwork@yahoo.com>
2022-11-22 16:44:07 -0800

    feat(#3bngwaj): make storybook bare bones running

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,47 +1,21 @@
-// .storybook/main.js
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook-addon-material-ui",
-    "@storybook-addon-gatsby",
-  ],
-  framework: "@storybook/react",
-  core: {
-    builder: "webpack5",
-  },
-  typescript: {
-    check: false,
-    checkOptions: {},
-    reactDocgen: "react-docgen-typescript",
-    reactDocgenTypescriptOptions: {
-      // speeds up storybook build time
-      allowSyntheticDefaultImports: false,
-      // speeds up storybook build time
-      esModuleInterop: false,
-      // makes union prop types like variant and size appear as select controls
-      shouldExtractLiteralValuesFromEnum: true,
-      // makes string and boolean types that can be undefined appear as inputs and switches
-      shouldRemoveUndefinedFromOptional: true,
-      // Filter out third-party props from node_modules except @mui packages
-      propFilter: (prop) =>
-        prop.parent
-          ? !/node_modules\/(?!@mui)/.test(prop.parent.fileName)
-          : true,
+    // You will want to change this to wherever your Stories will live
+    stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+    addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+    framework: "@storybook/react",
+    core: {
+        builder: "webpack5",
     },
-  },
-  webpackFinal: async (config) => {
-    // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
-    config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/];
-    // Remove core-js to prevent issues with Storybook
-    config.module.rules[0].exclude = [/core-js/];
-    // Use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
-    config.module.rules[0].use[0].options.plugins.push(
-      require.resolve("babel-plugin-remove-graphql-queries")
-    );
-    config.resolve.mainFields = ["browser", "module", "main"];
-    config.resolve.modules = ["node_modules", "stories"];
-    return config;
-  },
-};
+    webpackFinal: async config => {
+        // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
+        config.module.rules[0].exclude = [/node_modules\/(?!(gatsby|gatsby-script)\/)/]
+        // Remove core-js to prevent issues with Storybook
+        config.module.rules[0].exclude= [/core-js/]
+        // Use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
+        config.module.rules[0].use[0].options.plugins.push(
+            require.resolve("babel-plugin-remove-graphql-queries")
+        )
+        config.resolve.mainFields=["browser", "module", "main"]
+        return config
+    },
+}

49bf005fa82dee8aa33e3411fc97865d1bae26dc
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-11-24 00:33:57 +0100

    feat(CU-#3bv0tej): working on search Screen HomePage

--- src/screens/SearchScreen/index.tsx
+++ src/screens/SearchScreen/index.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 import { SearchLayout } from "@forkfacts/components";
+import { Box, Typography } from "@mui/material";
 
 const SearchScreen = () => {
   return (
     <SearchLayout>
-      <main>content</main>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          maxHeight: "100vh",
+          alignItems: "center",
+          marginTop: "130px",
+        }}
+      >
+        <Typography
+          component="h2"
+          sx={{
+            color: "#000000",
+            lineHeight: "32px",
+            textAlign: "center",
+            fontSize: "24px",
+            fontWeight: 600,
+          }}
+        >
+          Forkfacts, Your Healthy diet search place.
+        </Typography>
+      </Box>
     </SearchLayout>
   );
 };

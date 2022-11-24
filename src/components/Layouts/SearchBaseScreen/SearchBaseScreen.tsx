069500c2f5f6049e85da1f02759d83bb6998379e
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-11-24 21:36:55 +0100

    feat(CU-#3bv0tej): SearchBaseScreen done

--- src/components/Layouts/SearchBaseScreen/SearchBaseScreen.tsx
+++ src/components/Layouts/SearchBaseScreen/SearchBaseScreen.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useState } from "react";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { Box, TextField } from "@mui/material";
 import InputAdornment from "@mui/material/InputAdornment";
@@ -17,34 +17,66 @@ const useStyles = makeStyles({
 });
 
 const SearchBaseScreen: React.FC<PropsWithChildren> = ({ children }) => {
+  const [isSearchEmpty, setIsSearchEmpty] = useState<boolean>(true);
+  const [searchValue, setSearchValue] = useState<string>("");
   const classes = useStyles();
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.currentTarget.value.length > 0) {
+      setIsSearchEmpty(false);
+      setSearchValue(e.currentTarget.value);
+    } else setIsSearchEmpty(true);
+  };
+
+  const onCloseSearch = () => {
+    setIsSearchEmpty(!isSearchEmpty);
+    setSearchValue("");
+  };
+
   return (
-    <Box>
-      <Box sx={{ width: "100%" }}>
+    <div>
+      <Box
+        sx={{
+          boxShadow: isSearchEmpty ? "0px 1px 4px 0px #000000" : 0,
+          boxSizing: "border-box",
+          pt: "15px",
+          pb: "15px",
+          pl: "11px",
+          pr: "20px",
+        }}
+        style={{ width: "100%" }}
+      >
         <TextField
           id="filled-start-adornment"
+          type="text"
           sx={{ width: "100%", borderBottom: "none" }}
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
-                <ArrowBackIcon sx={{ width: "14px", height: "14px", color: "#356A1E" }} />
+                <ArrowBackIcon sx={{ color: "#356A1E", fontSize: "30px", py: "10px" }} />
               </InputAdornment>
             ),
             endAdornment: (
               <InputAdornment position="end">
-                <CloseIcon sx={{ width: "14px", height: "14px", color: "#356A1E" }} />
+                {isSearchEmpty || (
+                  <CloseIcon
+                    sx={{ color: "#356A1E", fontSize: "30px", py: "10px" }}
+                    onClick={() => onCloseSearch()}
+                  />
+                )}
               </InputAdornment>
             ),
             classes,
           }}
+          value={searchValue}
+          onChange={handleChange}
           placeholder="Search for food product"
           variant="standard"
         />
       </Box>
       {/* childern */}
       <Box>{children}</Box>
-    </Box>
+    </div>
   );
 };
 
--- src/screens/SearchScreen/components/SearchComponent.tsx
+++ src/screens/SearchScreen/components/SearchComponent.tsx
@@ -1,11 +1,12 @@
 import { SearchBaseScreen } from "@forkfacts/components";
+import { Box } from "@mui/system";
 import React from "react";
 
 const SearchComponent: React.FC = () => {
   return (
-    <>
-      <SearchBaseScreen></SearchBaseScreen>
-    </>
+    <div style={{ width: "100%" }}>
+      <SearchBaseScreen />
+    </div>
   );
 };
 

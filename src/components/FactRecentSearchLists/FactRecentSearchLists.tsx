74fb9809b8b851cf2d6bca85f927ea485742826e
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-11-27 08:11:52 +0100

    feat(CU-#3bv0tej): descriptive comments on theme typescript declarations

--- src/components/FactRecentSearchLists/FactRecentSearchLists.tsx
+++ src/components/FactRecentSearchLists/FactRecentSearchLists.tsx
@@ -14,28 +14,12 @@ interface propsTypes {
 
 const FactRecentSearchLists: React.FC<propsTypes> = ({ recentLists }) => {
   const theme = useTheme();
-  console.log(theme.appColor.success);
   return (
     <Box>
       {recentLists.map((item, index) => (
-        <Paper
-          elevation={0}
-          key={index}
-          sx={{ width: "100%", display: "flex", alignItems: "center", mb: 5 }}
-        >
+        <Paper elevation={0} key={index}>
           <img src={item.image} alt={item.name} style={{ width: "24px", height: "24px" }} />
-          <Typography
-            sx={{
-              ml: 2,
-              fontSize: (theme) => `${theme.spacing(2)}px`,
-              lineHeight: "24px",
-              verticallAign: "top",
-              letterSpacing: " 0.5px",
-              fontWeight: 400,
-            }}
-          >
-            {item.name}
-          </Typography>
+          <Typography>{item.name}</Typography>
         </Paper>
       ))}
     </Box>

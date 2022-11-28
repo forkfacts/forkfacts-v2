8d258fda28e3e6a0c0bf5ba4a9a79163c3c04465
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-11-28 18:50:20 +0100

    feat(CU-#3bv0tej):done with searchScreen homepage

--- src/screens/SearchScreens/SearchScreen.tsx
+++ src/screens/SearchScreens/SearchScreen.tsx
@@ -1,12 +1,10 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
+import { Box, TextField, AppBar, Toolbar, Typography, Theme } from "@mui/material";
+import { SearchOutlined } from "@mui/icons-material";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import { Theme } from "@mui/material";
 import { makeStyles } from "@mui/styles";
+import InputAdornment from "@mui/material/InputAdornment";
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
@@ -23,13 +21,32 @@ const useStyles = makeStyles((theme: Theme) => ({
     fontWeight: theme.typography.fontWeightBold,
     color: theme.palette.customGreen?.main,
   },
+  infoPage: {
+    display: "flex",
+    justifyContent: "center",
+    maxHeight: "100vh",
+    alignItems: "center",
+    flexDirection: "column",
+    width: "100%",
+    [theme.breakpoints.down("sm")]: {
+      marginTop: "150px",
+    },
+  },
+  mobilePageText: {
+    color: theme.palette.customBlack?.main,
+    lineHeight: "32px",
+    textAlign: "center",
+    fontSize: theme.typography.customFontSize.md.fontSize,
+    fontWeight: 600,
+    verticalAlign: "center",
+  },
 }));
 
 export default function SearchScreen() {
   const styles = useStyles();
   return (
     <Box className={styles.root}>
-      <AppBar color="transparent" sx={{ border: 0, boxShadow: "none" }}>
+      <AppBar color="transparent" sx={{ boxShadow: "none" }}>
         <Toolbar>
           <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 1 }}>
             <MenuIcon className={styles.icon} />
@@ -40,9 +57,32 @@ export default function SearchScreen() {
         </Toolbar>
       </AppBar>
       {/* content */}
-      <Box>
-        <Box>
-          <Typography></Typography>
+      <Box className={styles.infoPage}>
+        <Box sx={{ mt: "40px", width: "100%" }}>
+          <Typography component="h2" className={styles.mobilePageText}>
+            Forkfacts, Your Healthy diet search place.
+          </Typography>
+        </Box>
+        <Box sx={{ mt: "40px", width: "100%" }}>
+          <TextField
+            size="small"
+            placeholder="Search"
+            fullWidth
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchOutlined />
+                </InputAdornment>
+              ),
+            }}
+            sx={{
+              "& fieldset": {
+                paddingLeft: (theme) => theme.spacing(2.5),
+                borderRadius: "10px",
+                width: "100%",
+              },
+            }}
+          />
         </Box>
       </Box>
     </Box>

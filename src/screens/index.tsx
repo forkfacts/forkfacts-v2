0e5316e805caf39eb2633c4f09b7e947ceca5ac0
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-12-06 00:14:40 +0100

    feat(CU-#3bv0tej) improve code quality and refracturing updated

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,3 +1,3 @@
-export { default as HomeScreen } from "./SearchScreens/SearchHomeScreen";
+export { default as HomeScreen } from "./SearchScreens/HomeScreen";
 export { default as RecentSearchScreen } from "./SearchScreens/RecentSearch/RecentSearch";
 export { default as TypingSearchScreen } from "./SearchScreens/TypingSearchScreen/TypingSearchScreen";

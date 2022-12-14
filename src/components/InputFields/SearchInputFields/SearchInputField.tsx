ad43d828d1c9cf86b964d556f07e432a6fad969b
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-12-14 19:16:02 +0100

    ayomiku(#3ewa0rf) algolia auto search extra works in progress

--- src/components/InputFields/SearchInputFields/SearchInputField.tsx
+++ src/components/InputFields/SearchInputFields/SearchInputField.tsx
@@ -3,7 +3,7 @@ import {
   AutocompleteState,
   createAutocomplete,
 } from "@algolia/autocomplete-core";
-import { Box, TextField, Typography, useTheme } from "@mui/material";
+import { Box, Button, TextField, Typography, useTheme } from "@mui/material";
 import { SearchOutlined } from "@mui/icons-material";
 import InputAdornment from "@mui/material/InputAdornment";
 import { getAlgoliaResults } from "@algolia/autocomplete-preset-algolia";
@@ -12,6 +12,7 @@ import algoliasearch from "algoliasearch/lite";
 import React from "react";
 import { SearchResults } from "@forkfacts/components";
 import { SearchResultItemType } from "@forkfacts/models";
+import { ForLoops } from "@forkfacts/helpers";
 import { useStyles } from "./searchInputStyles";
 
 const searchClient = algoliasearch("latency", "6be0576ff61c053d5f9a3225e2a90f76");
@@ -49,6 +50,7 @@ function SearchInputField(props: Partial<AutocompleteOptions<AutocompleteItem>>)
       >({
         onStateChange({ state }) {
           setAutocompleteState(state);
+          console.log(state);
         },
         getSources() {
           return [
@@ -106,6 +108,8 @@ function SearchInputField(props: Partial<AutocompleteOptions<AutocompleteItem>>)
 
   const onSelectItem = (item: SearchResultItemType) => {};
 
+  const { query, collections, isOpen } = autocompleteState;
+
   return (
     <Box
       component="div"
@@ -141,25 +145,74 @@ function SearchInputField(props: Partial<AutocompleteOptions<AutocompleteItem>>)
             },
             borderRadius: spacing(1.25),
             py: spacing(1),
-            boxShadow: autocompleteState.isOpen ? shadows[0] : shadows[2],
+            boxShadow: isOpen ? shadows[0] : shadows[2],
           }}
           ref={inputRef}
           {...autocomplete.getInputProps({ inputElement: inputRef.current })}
         />
       </Box>
-      {autocompleteState.isOpen && (
-        <Box ref={panelRef} {...autocomplete.getPanelProps({})}>
-          <Box component="div">
-            {autocompleteState.collections.map((collection, index: number) => {
-              const { source, items } = collection;
-              return (
-                <Box component="section" key={`source-${index}`}>
-                  {items.length > 0 && (
-                    <SearchResults collectionListsItems={items} onSelectItem={onSelectItem} />
-                  )}
-                </Box>
-              );
-            })}
+      {isOpen && (
+        <Box
+          component="div"
+          ref={panelRef}
+          {...autocomplete.getPanelProps({})}
+          sx={{ mt: spacing(2), width: "100%" }}
+        >
+          {query === "" ? (
+            <Box
+              sx={{
+                mb: spacing(4),
+                width: "100%",
+                py: spacing(1.2),
+                paddingLeft: spacing(2),
+                paddingRight: spacing(3.9),
+              }}
+              component="div"
+            >
+              <Typography color="text.secondary" variant="body2">
+                I’m searching for
+              </Typography>
+            </Box>
+          ) : (
+            <Box
+              sx={{
+                mb: spacing(4),
+                width: "100%",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                py: spacing(1.2),
+                paddingLeft: spacing(2),
+                paddingRight: spacing(3.9),
+              }}
+              component="div"
+            >
+              <Typography color="text.secondary" variant="subtitle2">
+                Recent search
+              </Typography>
+              <Button
+                color="primary"
+                variant="text"
+                className={classes.clearBtn}
+                onClick={() => {}}
+              >
+                Clear all
+              </Button>
+            </Box>
+          )}
+          <Box>
+            <ForLoops each={collections}>
+              {(collection, index) => {
+                const { source, items } = collection;
+                return (
+                  <Box component="section" key={`source-${index}`}>
+                    {items.length > 0 && (
+                      <SearchResults collectionListsItems={items} onSelectItem={onSelectItem} />
+                    )}
+                  </Box>
+                );
+              }}
+            </ForLoops>
           </Box>
         </Box>
       )}
--- src/components/InputFields/SearchInputFields/searchInputStyles.tsx
+++ src/components/InputFields/SearchInputFields/searchInputStyles.tsx
@@ -1,8 +1,27 @@
 import { makeStyles } from "@mui/styles";
 import { Theme } from "@mui/material";
 
-export const useStyles = makeStyles(({}: Theme) => ({
+export const useStyles = makeStyles(({ breakpoints, spacing, typography }: Theme) => ({
   noBorder: {
     border: "none",
+  },
+  statusWrapper: {
+    [breakpoints.down("sm")]: {
+      paddingTop: spacing(1.2),
+      paddingLeft: spacing(2),
+      paddingBottom: spacing(1.2),
+      paddingRight: spacing(3.9),
+    },
+  },
+  clearBtn: {
+    [breakpoints.down("sm")]: {
+      fontWeight: typography.fontWeightBold,
+      fontSize: typography.caption.fontSize,
+      lineHeight: spacing(2),
+      textTransform: "capitalize",
+      textAlign: "center",
+      letterSpacing: spacing(0.005),
+      cursor: "pointer",
+    },
   },
 }));

faea11a8ffda91d1e3ccb038f70d0a02a04f473c
Ayomikuolatunji <69473765+Ayomikuolatunji@users.noreply.github.com>
2023-02-06 19:49:23 +0100

    CU_#860purrmq done with theme palette (#14)

---
+++ src/theme.d.ts
@@ -0,0 +1,37 @@
+import { createTheme } from "@mui/material";
+
+declare module "@mui/material/styles/createPalette" {
+  // allow configuration using `createTheme`
+  interface Palette {
+    tertiary: {
+      main: string;
+      light: string;
+      dark: string;
+      contrastText: string;
+    };
+    surface: {
+      main: string;
+      surface1: string;
+      surface2: string;
+      surface3: string;
+      surface4: string;
+      surface5: string;
+    };
+  }
+  interface PaletteOptions {
+    tertiary: {
+      main: string;
+      light: string;
+      dark: string;
+      contrastText: string;
+    };
+    surface: {
+      main: string;
+      surface1: string;
+      surface2: string;
+      surface3: string;
+      surface4: string;
+      surface5: string;
+    };
+  }
+}

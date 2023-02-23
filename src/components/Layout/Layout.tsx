b27b77d4fd6e6709e2edc08822606469d26437cc
Ayomikuolatunji <69473765+Ayomikuolatunji@users.noreply.github.com>
2023-02-23 21:15:18 +0100

    remove fomts

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,53 +4,52 @@ import { ThemeProvider } from "@mui/material/styles";
 import { customTheme } from "../../themes/theme";
 import { Header, SideBar } from "@forkfacts/components";
 import { LayoutProps } from "@forkfacts/models";
-import "@fontsource/open-sans";
-import "@fontsource/open-sans/500.css";
+
 
 const Layout: FC<LayoutProps> = ({ children, sidebarItems }) => {
   const theme = useTheme();
-  const [drawerWidth, setDrawerWidth] = useState(theme.spacing(12.25));
-  const [drawerWidthExpanded, setDrawerWidthExpanded] = useState(false);
-  const [mobileOpen, setMobileOpen] = useState(false);
+    const [drawerWidth, setDrawerWidth] = useState(theme.spacing(12.25));
+      const [drawerWidthExpanded, setDrawerWidthExpanded] = useState(false);
+        const [mobileOpen, setMobileOpen] = useState(false);
 
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-    if (!mobileOpen) {
-      setDrawerWidth(theme.spacing(28.25));
-      setDrawerWidthExpanded(true);
-    } else {
-      setDrawerWidth(theme.spacing(16.5));
-      setDrawerWidthExpanded(false);
-    }
-  };
+          const handleDrawerToggle = () => {
+              setMobileOpen(!mobileOpen);
+                  if (!mobileOpen) {
+                        setDrawerWidth(theme.spacing(28.25));
+                              setDrawerWidthExpanded(true);
+                                  } else {
+                                        setDrawerWidth(theme.spacing(16.5));
+                                              setDrawerWidthExpanded(false);
+                                                  }
+                                                    };
 
-  return (
-    <ThemeProvider theme={customTheme}>
-      <Box>
-        <Header handleToggleButton={handleDrawerToggle} />
-      </Box>
-      <Box sx={{ display: { sm: "flex", xs: "block" } }}>
-        <CssBaseline />
-        <SideBar
-          handleDrawerToggle={handleDrawerToggle}
-          mobileOpen={mobileOpen}
-          drawerWidth={drawerWidth}
-          sidebarItems={sidebarItems}
-          drawerWidthExpanded={drawerWidthExpanded}
-        />
-        <Box
-          sx={{
-            width: {
-              md: `calc(100% - ${drawerWidth}px)`,
-              xs: "100%",
-            },
-          }}
-        >
-          {children}
-        </Box>
-      </Box>
-    </ThemeProvider>
-  );
-};
+                                                      return (
+                                                          <ThemeProvider theme={customTheme}>
+                                                                <Box>
+                                                                        <Header handleToggleButton={handleDrawerToggle} />
+                                                                              </Box>
+                                                                                    <Box sx={{ display: { sm: "flex", xs: "block" } }}>
+                                                                                            <CssBaseline />
+                                                                                                    <SideBar
+                                                                                                              handleDrawerToggle={handleDrawerToggle}
+                                                                                                                        mobileOpen={mobileOpen}
+                                                                                                                                  drawerWidth={drawerWidth}
+                                                                                                                                            sidebarItems={sidebarItems}
+                                                                                                                                                      drawerWidthExpanded={drawerWidthExpanded}
+                                                                                                                                                              />
+                                                                                                                                                                      <Box
+                                                                                                                                                                                sx={{
+                                                                                                                                                                                            width: {
+                                                                                                                                                                                                          md: `calc(100% - ${drawerWidth}px)`,
+                                                                                                                                                                                                                        xs: "100%",
+                                                                                                                                                                                                                                    },
+                                                                                                                                                                                                                                              }}
+                                                                                                                                                                                                                                                      >
+                                                                                                                                                                                                                                                                {children}
+                                                                                                                                                                                                                                                                        </Box>
+                                                                                                                                                                                                                                                                              </Box>
+                                                                                                                                                                                                                                                                                  </ThemeProvider>
+                                                                                                                                                                                                                                                                                    );
+                                                                                                                                                                                                                                                                                    };
 
-export default Layout;
+                                                                                                                                                                                                                                                                                    export default Layout;

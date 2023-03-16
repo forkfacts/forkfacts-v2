e5f899337ce5e8c27d32c6b14bcc2f884d47249c
Ayomikuolatunji <69473765+Ayomikuolatunji@users.noreply.github.com>
2023-03-16 22:12:23 +0100

    CU_#860q6d11w added script to download data files (#20)
    * CU_#860q6d11w added script to download data files
    
    * CU_#860q6d11w added script to download data files
    
    * CU_#860q6d11w convert file to typescript, data directory to root directory
    
    * CU_#860q6d11w convert file to typescript, data directory to root directory
    
    * CU_#860q6d11w convert file to typescript, data directory to root directory
    
    * CU_#860q6d11w added .env variable
    
    * CU_#860q6d11w added .env variable and README documentation
    
    * CU_#860q6d11w added .env variable and README documentation
    
    * CU_#860q6d11w added .env variable and README documentation
    
    * CU_#860q6d11w added .env variable and README documentation
    
    * CU_#860q6d11w added .env variable and README documentation
    
    * CU_#860q6d11w fixed deployment issue
    
    * CU_#860q6d11w fixed deployment issue
    
    * feat(#860q6d11w): change the location of the script
    
    * CU_#860q6d11w remove type:'module'
    
    * CU_#860q6d11w convert to .mjs format
    
    ---------
    
    Co-authored-by: hhimanshu <dev.atwork@yahoo.com>

---
+++ scripts/init.mjs
@@ -0,0 +1,44 @@
+import { $ } from "zx";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const files = [
+  {
+    name: "foundation_food_nutrition_facts.json",
+  },
+  {
+    name: "sr_legacy_food_nutrition_facts.json",
+  },
+  {
+    name: "usda_rdi_nutrient_mapping.json",
+  },
+  { name: "rdi.json" },
+];
+
+/**
+ * Create the nested directories, if needed, and downloads the file from the server and use it to create file directory.
+ * @param url
+ * @param toDir
+ * @param fileName
+ */
+
+const download = async (url, toDir, fileName) => {
+  return $`rm -rf ${toDir} && mkdir -p ${toDir} && wget ${url} -O "${toDir}/${fileName}"`;
+};
+
+async function downloadFiles() {
+  try {
+    await $`cd src`;
+    await $`mkdir -p data`;
+    for (const file of files) {
+      const fileUrl = `${process.env.DATA_ENDPOINT}/${file.name}`;
+      await download(fileUrl, "data", file.name);
+    }
+    console.log("Downloaded successfully");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+downloadFiles();

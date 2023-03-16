5a2f6ad1e74260a569d329254a7f27b392c03f7b
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2023-03-16 21:03:37 +0100

    CU_#860q6d11w added .env variable

--- scripts.ts
+++ scripts.ts
@@ -7,33 +7,33 @@ const files = [
   {
     name: "foundation_food_nutrition_facts.json",
   },
-  { name: "rdi.json" },
   {
     name: "sr_legacy_food_nutrition_facts.json",
   },
   {
     name: "usda_rdi_nutrient_mapping.json",
   },
+  { name: "rdi.json" },
 ];
 
 /**
- * Create the nested directories, if needed, and downloads the file from internet
+ * Create the nested directories, if needed, and downloads the file from the server and use it to create file directory.
  * @param url
  * @param toDir
  * @param fileName
  */
 
 const download = async (url: string, toDir: string, fileName: string) => {
-  return $`mkdir -p ${toDir} && wget ${url} -O "${toDir}/${fileName}"`;
+  return $`rm -rf ${toDir} && mkdir -p ${toDir} && wget ${url} -O "${toDir}/${fileName}"`;
 };
 
 async function downloadFiles() {
   try {
-    dotenv;
     await $`cd src`;
     await $`mkdir -p data`;
     for (const file of files) {
-      download(process.env.DATA_ENDPOINT as string, "data", file.name);
+      const fileUrl = `${process.env.DATA_ENDPOINT as string}/${file.name}`;
+      download(fileUrl, "data", file.name);
     }
     console.log("Downloaded successfully");
   } catch (error) {

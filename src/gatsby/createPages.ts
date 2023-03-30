d17ea4469502505ca014ff7bbfe7dbe256c62674
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-03-30 07:53:51 +0100

    refactoring search nutrition filter function

--- src/gatsby/createPages.ts
+++ src/gatsby/createPages.ts
@@ -1,6 +1,6 @@
 // gatsby-node.js
 import { GatsbyNode } from "gatsby";
-import { createDetailPage } from "../pageGenerators/DetailsPageGenerator";
+import { createDetailPage } from "../generators/DetailsPageGenerator";
 
 export const createPages: GatsbyNode["createPages"] = async ({ graphql, actions }) => {
   const { createPage } = actions;

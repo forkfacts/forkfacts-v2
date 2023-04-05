28ddf3dbed3b17a738944970812d64eafa0e7b7b
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-04-05 18:04:59 +0100

    CU_#860qewjwn SEO content improved

--- src/generators/DetailsPageGenerator.ts
+++ src/generators/DetailsPageGenerator.ts
@@ -6,21 +6,31 @@ export const spaceToDashes = (name: string) => {
   return pathname.endsWith("-") ? pathname.substr(0, pathname.length - 1) : pathname;
 };
 
-export const generateSEOTitle = (foodName: string) => {
-  const name = foodName.replace(/\s*\([^)]*\)\s*/g, "");
-  return `Nutrition facts - ${name}`;
-};
-
-export const generateSEOMetaDescription = (foodName: string, category: string) => {
-  return `Nutrient facts from USDA and NIH with the option to filter by nutrients, age and gender for 100gm of  ${foodName} in ${category} category.`;
-};
-
 const path = require("path");
-
 const ff_nutrition_facts = require("../../data/foundation_food_nutrition_facts.json");
 const sr_legacy_nutrition_facts = require("../../data/sr_legacy_food_nutrition_facts.json");
-
 const recommendedDailyIntakes = require("../../data/rdi.json");
+interface Food {
+  category: string;
+  name: string;
+  fdcId: number;
+  nutrients: {
+    amount: number;
+    name: string;
+    unit: string;
+    displayName: string;
+    nutrientGroup: string;
+  }[];
+}
+
+const ENERGY_NAME = "Energy (Atwater General Factors)";
+const ENERGY_UNIT = "KCAL";
+const FAT_NAME = "Total lipid (fat)";
+const FAT_UNIT = "G";
+const CARBOHYDRATE_NAME = "Carbohydrate, by difference";
+const CARBOHYDRATE_UNIT = "G";
+const PROTEIN_NAME = "Protein";
+const PROTEIN_UNIT = "G";
 
 export const createDetailPage = (createPage: any) => {
   try {
@@ -44,10 +54,33 @@ const createNutritionTable = ({ createPageFunction, foods, indexFileName }: any)
   const template = path.resolve("src/templates/DetailsPage.tsx");
   foods.forEach((food: any) => {
     if (food.name) {
+      function filterNutrient(food: Food, name: string, unit: string): number {
+        const nutrient = food.nutrients.find(
+          (nutrient) => nutrient.name === name && nutrient.unit === unit
+        );
+        return nutrient ? nutrient.amount : 0;
+      }
+      function generateSEOInfo(
+        name: string,
+        calories: number,
+        protein: number,
+        carbohydrates: number,
+        fat: number
+      ): { title: string; description: string } {
+        const title = `${name} Nutrition Facts: Calories, Protein, Carbs, and Fat`;
+        const description = `Learn about the nutrition facts of ${name}. ${calories} calories, ${protein}g protein, ${carbohydrates}g carbs, and ${fat}g fat per serving.`;
+        return { title, description };
+      }
+      const calories = filterNutrient(food, ENERGY_NAME, ENERGY_UNIT);
+      const fat = filterNutrient(food, FAT_NAME, FAT_UNIT);
+      const carbohydrates = filterNutrient(food, CARBOHYDRATE_NAME, CARBOHYDRATE_UNIT);
+      const protein = filterNutrient(food, PROTEIN_NAME, PROTEIN_UNIT);
+      const seoInfo = generateSEOInfo(food.name, calories, protein, carbohydrates, fat);
+
       const pagePath = spaceToDashes(food["name"].toString());
       const seo: any = {
-        title: generateSEOTitle(food.name),
-        description: generateSEOMetaDescription(food.name, food.category),
+        title: seoInfo.title,
+        description: seoInfo.description,
         pagePath,
       };
       createPageFunction({

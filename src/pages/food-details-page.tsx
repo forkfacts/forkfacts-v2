2be7024a77357a3f316bbe468c5bf16c250e45c9
hhimanshu <dev.atwork@yahoo.com>
2023-05-26 17:13:25 -0700

    feat(): update path

--- src/pages/food-details-page.tsx
+++ src/pages/food-details-page.tsx
@@ -1,5 +1,5 @@
+import FoodDetails from "../Features/FoodDetails";
 import { App } from "konsta/react";
-import FoodDetails from "../features/FoodDetails";
 import React from "react";
 
 const FoodDetailsPage = () => {

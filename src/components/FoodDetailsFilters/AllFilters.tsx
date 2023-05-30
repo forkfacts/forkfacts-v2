4c5b6a37eb6b92f574b79c9a88c3122473c63199
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-05-30 20:04:49 +0100

    fixed tailwindcss themes bugs and typography

--- src/components/FoodDetailsFilters/AllFilters.tsx
+++ src/components/FoodDetailsFilters/AllFilters.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { BsFilter } from "react-icons/bs";
+import { BiFilter } from "react-icons/bi";
 
 const AllFilters = () => {
   return (
     <div>
       <button className="w-[34px] h-[34px] text-[#47464F] border-[1px] border-[#E5E1E6] rounded-[8px] flex justify-center items-center box-border">
-        <BsFilter className="text-[#47464F] w-[18px] h-[18px]" />
+        <BiFilter className="text-[#47464F] w-[24px] h-[24px]" />
       </button>
     </div>
   );

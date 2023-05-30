245e584b57a7ec720d254a3ce38ef5619ce9156d
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-05-30 14:38:19 +0100

    checking....2

--- src/pages/food-details-page.tsx
+++ src/pages/food-details-page.tsx
@@ -1,12 +1,8 @@
 import React from "react";
-import FoodDetails from "../features/FoodDetails/FoodDetails";
+// import FoodDetails from "../features/FoodDetails/FoodDetails";
 
 const FoodDetailsPage = () => {
-  return (
-    <div>
-      <FoodDetails />
-    </div>
-  );
+  return <div>{/* <FoodDetails /> */}</div>;
 };
 
 export default FoodDetailsPage;

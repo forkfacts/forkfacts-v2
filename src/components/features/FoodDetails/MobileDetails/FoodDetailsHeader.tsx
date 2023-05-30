950550b16aba340b3a79a4c9d1bf599d9e4d669b
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-05-30 16:23:35 +0100

    header review done

--- src/components/features/FoodDetails/MobileDetails/FoodDetailsHeader.tsx
+++ src/components/features/FoodDetails/MobileDetails/FoodDetailsHeader.tsx
@@ -19,7 +19,7 @@ const DetailsHeader = () => {
       title="Banana, overripe, raw"
       centerTitle
       className="top-0 fixed left-0 right-0 p-3"
-      titleClassName="text-[22px] text-main font-[500] leading-[20px]"
+      titleClassName="text-[20px] font-[500] leading-[20px] text-[#1C1B1F] tracking-[0.1px]"
       bgClassName="bg-white"
     />
   );

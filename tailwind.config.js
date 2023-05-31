67c4dcca533f97413bedf86e738b385590f5bc68
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-05-31 01:21:36 +0100

    fixed issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,8 +23,6 @@ module.exports = konstaConfig({
     borderRadius: {
       none: "0",
       sm: "0.125rem",
-      DEFAULT: "0.25rem",
-      DEFAULT: "4px",
       md: "0.375rem",
       lg: "36px",
       full: "9999px",

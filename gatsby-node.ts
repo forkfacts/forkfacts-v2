6bb90dd8e0aa7a579ca96faf86418c777e2b5cae
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-06-23 22:42:35 +0100

    production fixing

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,18 +1,3 @@
 import { createPages } from "./src/gatsby/createPages";
 
 exports.createPages = createPages;
-
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }: any) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /offending-module/,
-            use: loaders.null(),
-          },
-        ],
-      },
-    });
-  }
-};
--- src/components/Offline/Offline.tsx
+++ src/components/Offline/Offline.tsx
@@ -3,47 +3,49 @@ import { RiWifiOffLine } from "react-icons/ri";
 import { Button } from "konsta/react";
 import { navigate } from "gatsby";
 
-const isBrowser = typeof window !== "undefined";
+const Offline = () => {
+  const [isOnline, setIsOnline] = useState(
+    typeof window !== "undefined" && window && navigator.onLine
+  );
 
-const OfflinePage = () => {
-  const [isOnline, setIsOnline] = useState(true);
+  useEffect(() => {
+    if (isOnline) {
+      navigate("/");
+    }
+  }, [isOnline]);
 
   useEffect(() => {
     const handleConnectionChange = () => {
-      if (isBrowser) {
-        setIsOnline(window && window.navigator.onLine);
+      if (typeof window !== "undefined") {
+        setIsOnline(navigator.onLine);
       }
     };
-    if (isBrowser) {
-      setIsOnline(window.navigator.onLine);
-      window && window.addEventListener("online", handleConnectionChange);
-      window && window.addEventListener("offline", handleConnectionChange);
+
+    if (typeof window !== "undefined") {
+      window.addEventListener("online", handleConnectionChange);
+      window.addEventListener("offline", handleConnectionChange);
     }
 
     return () => {
-      if (isBrowser) {
-        window && window.removeEventListener("online", handleConnectionChange);
-        window && window.removeEventListener("offline", handleConnectionChange);
+      if (typeof window !== "undefined") {
+        window.removeEventListener("online", handleConnectionChange);
+        window.removeEventListener("offline", handleConnectionChange);
       }
     };
   }, []);
 
   const handleRetry = () => {
-    if (isBrowser) {
-      window && window.location.reload();
+    if (typeof window !== "undefined") {
+      window.location.reload();
     }
   };
-
-  if (isOnline) {
-    navigate("/");
-  }
 
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="text-center flex flex-col justify-center items-center">
         <RiWifiOffLine className="w-12 h-12 text-primary-40 mb-4" />
-        <h1 className="text-textDark font-500 text-[16px]">You are offline</h1>
-        <p className="w-64 text-center text-dark mt-2 mb-6 font-400">
+        <h1 className="text-main font-semibold text-2xl">You are offline</h1>
+        <p className="w-64 text-center text-main mt-5 mb-6">
           You are currently offline. Please check your internet connection.
         </p>
         <Button onClick={handleRetry}>Try again</Button>
@@ -52,4 +54,4 @@ const OfflinePage = () => {
   );
 };
 
-export default OfflinePage;
+export default Offline;

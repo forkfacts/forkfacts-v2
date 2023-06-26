3faace7d69f142cc6c970f9ba162ae51b3945a3f
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-06-26 12:12:35 +0100

    icon fixes 5

--- gatsby-ssr.jsx
+++ gatsby-ssr.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { App } from "konsta/react";
 import "@fontsource/poppins";
 import "@algolia/autocomplete-theme-classic";
@@ -9,25 +9,7 @@ import "./src/styles/styles.css";
 import { Helmet } from "react-helmet";
 import { HelmetProvider } from "react-helmet-async";
 
-const isBrowser = typeof window !== "undefined";
-
 const WrapPageElement = ({ element }) => {
-  useEffect(() => {
-    if (isBrowser) {
-      window &&
-        window.addEventListener("offline", function (e) {
-          navigate("/offline");
-        });
-    }
-    return () => {
-      if (isBrowser) {
-        window &&
-          window.removeEventListener("offline", function (e) {
-            navigate("/offline");
-          });
-      }
-    };
-  }, []);
   return (
     <HelmetProvider>
       <Helmet defer={false} />

4ab65ff1d5635e71f15d1c9a1097fe0a0e3c91ff
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-06-26 11:59:29 +0100

    icon update

--- gatsby-ssr.jsx
+++ gatsby-ssr.jsx
@@ -44,13 +44,3 @@ const WrapPageElement = ({ element }) => {
 };
 
 export const wrapRootElement = WrapPageElement;
-
-export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    "This application has been updated. Reload to display the latest version?"
-  );
-  if (answer === true) {
-    window.location.reload();
-  }
-};
-export const registerServiceWorker = () => true;

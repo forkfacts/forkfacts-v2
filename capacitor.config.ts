196112bdab22cbd469c09b9a5b2cb3a7ea3441c8
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-06-27 17:56:30 +0100

    fixed capacitor environment variable load issue

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,16 @@
 import { CapacitorConfig } from "@capacitor/cli";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: CapacitorConfig = {
-  appId: "com.example.app",
-  appName: "forkfacts",
+  appId: "com.forkfacts.app",
+  appName: "Forkfacts",
+  webDir: "www",
+  bundledWebRuntime: false,
   server: {
-    androidScheme: "https",
-    url: "https://forkfacts-v2-jn4gw4u3g-bonsaiilabs.vercel.app/",
+    url: process.env.GATSBY_APP_URL,
+    cleartext: true,
   },
 };
 
